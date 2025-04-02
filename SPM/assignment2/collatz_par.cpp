37f13f39b833508e942b83d97d51a5a7104ee6fc
mirdan08 <mirdan0825@gmail.com>
2025-04-02 22:38:09 +0200

    [SPM] fixed collat parallel implementation

--- SPM/assignment2/collatz_par.cpp
+++ SPM/assignment2/collatz_par.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<utility>
 #include<vector>
+#include<deque>
 #include<string>
 #include<algorithm>
 #include<cmath>
@@ -12,13 +13,13 @@
 int main(int argc,char* argv[]){
     if(argc < 2){
         std::cout << "Usage: " << argv[0] << " [-d] [-n num_threads] [-c chunk_size] range1 [range2 ...]\n"
-        << "Options:\n"
-        << "\t-d                Enable dynamic scheduling (default is block-cyclic)\n"
-        << "\t-n num_threads    Set the number of threads (default is 16)\n"
-        << "\t-c chunk_size     Set the block/task size (default is 1)\n"
-        << "\trange1, range2,... Specify ranges in the form 'start-end' (e.g. 1-100)\n"
-        << "\n"
-        << "Example: " << argv[0] << " -d -n 8 -c 32 1-100 200-300\n";
+                << "Options:\n"
+                << "\t-d                Enable dynamic scheduling (default is block-cyclic)\n"
+                << "\t-n num_threads    Set the number of threads (default is 16)\n"
+                << "\t-c chunk_size     Set the block/task size (default is 32)\n"
+                << "\trange1, range2,... Specify ranges in the form 'start-end' (e.g. 1-100)\n"
+                << "\n"
+                << "Example: " << argv[0] << " -d -n 8 -c 32 1-100 200-300\n";
         return 0;
     }
     bool doneParsing=false;
@@ -28,6 +29,7 @@ int main(int argc,char* argv[]){
     int threadsNumber=8;
     int taskSize=32;
     
+    //arguments parsing
     while(!doneParsing){
         std::string current=argv[i];
 
@@ -46,6 +48,7 @@ int main(int argc,char* argv[]){
         i++;
     }
     
+    //ranges parsing
     std::vector<std::pair<int,int>> ranges;
     
     std::transform( argv +i-1,argv+argc, std::back_inserter(ranges),parseRange);
@@ -53,23 +56,103 @@ int main(int argc,char* argv[]){
     
     std::cout << ranges.front().first  << " " << ranges.back().second << std::endl;
     
+    //array containing the final computation of each range
     std::vector<unsigned long long> maxRange(rangesNumber,0);
     
-    std::mutex mutex;
-
-    auto blockCyclicDistribution=[&](int threadIdx)->std::vector<unsigned long long>{   
+    std::function<std::vector<unsigned long long>(int)> blockCyclicDistribution=[&](int threadIdx)->std::vector<unsigned long long>{   
         std::vector<unsigned long long> partialMaxRange(rangesNumber,0);    
         for( int i=0;i<rangesNumber; i++ ){
             const auto& [start,end] = ranges[i];
             int rangeSize=end-start+1;
             int rangeBlocksNumber =  std::floor(rangeSize+taskSize-1)/taskSize;
-            unsigned long long partialMax=0;
+
+
             for(int t=threadIdx;t<rangeBlocksNumber;t+=threadsNumber){
                 int taskStart=start+t*taskSize;
                 int taskEnd = std::min(taskStart + taskSize - 1, end);
                 partialMaxRange[i]=std::max(partialMaxRange[i],partialMaxCollatzStep(taskStart,taskEnd));
             }
         }
+        
+        return partialMaxRange;
+        
+    };
+    
+    //dynamic scheduling shared variables
+    std::mutex chunksMutex;
+    int bufferSize=std::max(threadsNumber+1,(int)ranges.size());
+
+    //fixed size circular buffer
+    int bufferReadIdx=0;
+    int bufferWriteIdx=0;
+    std::vector <std::pair<int,int>> availableChunks(bufferSize,{-1,-1});
+
+    for(int i=0;i<std::min((int)ranges.size(),bufferSize);i++){
+        //initializare with the starting chunk of each range
+        availableChunks[i].first=i;
+        availableChunks[i].second=0;
+    }
+
+    bufferWriteIdx=std::min((int)ranges.size(),bufferSize);
+    int remainingRanges= ranges.size();
+    std::function<std::vector<unsigned long long>(int)> dynamicDistribution=[&](int threadIdx)->std::vector<unsigned long long>{   
+
+        std::vector<unsigned long long> partialMaxRange(rangesNumber,0);   
+
+        int currTaskIdx=-1;
+        int currTaskChunk=-1;
+
+        unsigned long long chunkStart=-1;
+        unsigned long long chunkEnd=-1;
+        bool terminate=false;
+
+        while(true){
+            {
+                std::lock_guard<std::mutex> lock_guard(chunksMutex);
+                //we check if there is work to do
+                if(remainingRanges==0){
+                    terminate=true;
+                    break;
+                }
+
+                //we are sure that there is work to do
+                const auto& [taskIdx,taskChunk]=availableChunks[bufferReadIdx];
+                bufferReadIdx=(bufferReadIdx+1)%availableChunks.size();
+                if((taskChunk+1)*taskSize+1 < ranges[taskIdx].second){
+                    // next chunk is available -> add it to queue
+                    availableChunks[bufferWriteIdx].first=taskIdx;
+                    availableChunks[bufferWriteIdx].second=taskChunk+1;
+                    
+                    bufferWriteIdx=(bufferWriteIdx+1)%availableChunks.size();
+                }
+                else{
+                    // next chunk is not available -> this range is terminated
+                    remainingRanges--;
+                }
+
+                //save the extracted task data
+                currTaskIdx=taskIdx;
+                currTaskChunk=taskChunk;
+            }  
+            //if work is done exit
+            if (terminate == true){
+                break;
+            }
+
+            //process the element
+            
+            chunkStart= ranges[currTaskIdx].first+currTaskChunk*taskSize;
+            chunkEnd=std::min(
+                ranges[currTaskIdx].first+(currTaskChunk+1)*taskSize,
+                ranges[currTaskIdx].second
+            );
+            
+            partialMaxRange[currTaskIdx]=std::max(
+                partialMaxRange[currTaskIdx],
+                partialMaxCollatzStep(chunkStart,chunkEnd)
+            );
+        }
+
 
         return partialMaxRange;
         
@@ -79,7 +162,8 @@ int main(int argc,char* argv[]){
     std::vector<std::future<std::vector<unsigned long long>>> futures;
 
     for(int t=0;t<threadsNumber;t++){
-        std::packaged_task<std::vector<unsigned long long>(int threadIdx)> task(blockCyclicDistribution);
+        std::function<std::vector<unsigned long long>(int)> schedulingStrategy = dynamicScheduling? dynamicDistribution:blockCyclicDistribution;
+        std::packaged_task<std::vector<unsigned long long>(int)> task(schedulingStrategy);
         futures.emplace_back(task.get_future());
         std::thread thread(std::move(task),t);
         threads.emplace_back(std::move(thread));
