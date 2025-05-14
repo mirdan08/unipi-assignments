f5f63ff3444ba9f2e27383626fec39516d63bd92
mirdan08 <mirdan0825@gmail.com>
2025-05-14 22:59:44 +0200

    [SPM] first sequential full implementation (tbc)

--- SPM/assignment4/mergeSortSeq.cpp
+++ SPM/assignment4/mergeSortSeq.cpp
@@ -6,14 +6,16 @@
 #include<algorithm>
 #include <cstdlib>
 #include <getopt.h>
+#include <chrono>
 
 int main(char*argv[],int argc){
 
     size_t arraySize=100;
     size_t recordSize = 0;
     int numThreads = 0;
+    bool verboseOutput=false;
     int opt;
-    while ((opt = getopt(argc, argv, "s:r:t:")) != -1) {
+    while ((opt = getopt(argc, argv, "s:r:t:v:")) != -1) {
         switch (opt) {
             case 's':
             arraySize = std::stoull(optarg);
@@ -24,6 +26,8 @@ int main(char*argv[],int argc){
             case 't':
             numThreads = std::stoi(optarg);
             break;
+            case 'v':
+            verboseOutput=true;
             default:
             return EXIT_FAILURE;
         }
@@ -36,10 +40,16 @@ int main(char*argv[],int argc){
     std::transform(dataVector.begin(), dataVector.end(), dataVector.begin(),
                    [&](int){ return dis(gen);}
             );
-    
-    
-
-    
-    
-
+            
+    auto startTime = std::chrono::high_resolution_clock::now();
+    sequentialMergeSort(dataVector,0,dataVector.size());
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    std::cout<< "time(s):" << duration.count() << std::endl;
+    if(verboseOutput){
+        unsigned int i=1;
+        for(const auto& v:dataVector){
+            std::cout << i++ << ":" << v << std::endl;
+        }
+    }
 }
