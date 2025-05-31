9b671fa5d2ebff1352a00a29aef76960dbbbad26
mirdan08 <mirdan0825@gmail.com>
2025-05-31 20:12:55 +0200

    [SPM] added class declarations to header

--- SPM/assignment4/mergesort.hpp
+++ SPM/assignment4/mergesort.hpp
@@ -2,6 +2,8 @@
 #include <ff/ff.hpp>
 #include <mpi.h>
 
+#ifndef MERGESORT_HPP
+#define MERGESORT_HPP
 const unsigned int RPAYLOAD=1;
 
 struct Record { 
@@ -9,20 +11,28 @@ struct Record {
     char rpayload[RPAYLOAD]; 
 }; 
 
+using OrderedArray=std::vector<Record>;
+using task_t=std::pair<OrderedArray*,OrderedArray*>;
+
+using PosKeyPair=std::pair<unsigned long,unsigned long>;
+using PosKeyVec=std::vector<PosKeyPair>;
+using Task_t=std::pair<PosKeyVec*,PosKeyVec*>;
+
 void sequentialMerge(std::vector<Record>& arr, int left, int mid, int right);
 
 void sequentialMergeSort(std::vector<Record>& arr, int left, int right);
 
-class StreamParser;
-
-class MasterNode;
+struct MasterNode;
 
 struct Worker;
 
-using OrderedArray=std::vector<Record>;
-using task_t=std::pair<OrderedArray*,OrderedArray*>;
-
-
-void parallelFFMergeSort(std::vector<Record>* src,OrderedArray** dst,size_t numThreads,size_t leafSize);
-
+std::unique_ptr<ff::ff_pipeline> spawn_ff_merging_pipeline(
+    std::vector<Record>& src,
+    PosKeyVec** dst,
+    size_t numWorkers,
+    size_t numSorters,
+    size_t leafSize
+);
 std::vector<Record> spawn_datastream(size_t arraySize,size_t recordSize,unsigned int seed=42);
+
+#endif
