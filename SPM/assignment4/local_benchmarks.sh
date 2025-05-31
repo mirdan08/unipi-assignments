435d75f422826d29e7461c2c78b65a5773a234a7
mirdan08 <mirdan0825@gmail.com>
2025-05-31 22:58:48 +0200

    [SPM] added becnhmarking scripts

--- SPM/assignment4/local_benchmarks.sh
+++ SPM/assignment4/local_benchmarks.sh
@@ -9,23 +9,22 @@
 
 leaf_size=(100 10000 100000)
 num_threads=(1 2 4 8 16 32)
-node_size=(2 3 4 5 6 7 8)
-#array_size=(1000000 10000000)
-array_size=(1000000 10000000)
-payload_size=(100)
+node_size=(1 2 3 4 5 6 7 8)
+array_size=(1000000 10000000 1000000000 1000000000)
+payload_size=(100 1000 10000)
 trials=5
 
-make cleanall
-make mergeSortPar
-make sortSeq
-make mergeSortDist
 
 echo "iteration,leaf_size,num_threads,payload_size,array_size,mpi_nodes,ff_par_time,sort_time,dist_time"
 
 for i in $(seq 1 $trials); do
-    for nn in "${node_size[@]}"; do
-        for as in "${array_size[@]}"; do
-            for ps in "${payload_size[@]}"; do
+    for ps in "${payload_size[@]}"; do
+        make cleanall >/dev/null 2>&1
+        make mergeSortPar RPAYLOAD=$ps >/dev/null 2>&1
+        make sortSeq RPAYLOAD=$ps >/dev/null 2>&1
+        make mergeSortDist RPAYLOAD=$ps >/dev/null 2>&1
+        for nn in "${node_size[@]}"; do
+            for as in "${array_size[@]}"; do
                 for n in "${num_threads[@]}"; do
                     sort_output="$(./sortSeq -s $as -r $ps)"
                     sort_time=$(echo "$sort_output" | tail -n 1 | cut -d':' -f2)
@@ -41,6 +40,7 @@ for i in $(seq 1 $trials); do
                         echo "$i,$ls,$n,$ps,$as,$nn,$ff_par_time,$sort_time,$dist_par_time"
                     done
                 done
+                echo "array size:$as" 
             done
         done
     done
